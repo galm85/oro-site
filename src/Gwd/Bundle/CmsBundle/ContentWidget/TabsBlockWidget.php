6ef7ba421a6f22e972e39872ce5fe1d76c1570da
Gal <galm85@gmail.com>
2026-02-11 16:12:39 +0200

    create widget with content blocks

--- src/Gwd/Bundle/CmsBundle/ContentWidget/TabsBlockWidget.php
+++ src/Gwd/Bundle/CmsBundle/ContentWidget/TabsBlockWidget.php
@@ -2,20 +2,24 @@
 
 namespace Gwd\Bundle\CmsBundle\ContentWidget;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Oro\Bundle\CMSBundle\ContentWidget\AbstractContentWidgetType;
+use Oro\Bundle\CMSBundle\Entity\ContentBlock;
 use Oro\Bundle\CMSBundle\Entity\ContentWidget;
-use Oro\Bundle\CMSBundle\Form\Type\WYSIWYGValueType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Oro\Bundle\CMSBundle\Form\Type\ContentBlockSelectType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Form\FormInterface;
 use Twig\Environment;
 
-
-
-
 class TabsBlockWidget extends AbstractContentWidgetType
 {
+    private ManagerRegistry $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
 
     public static function getName(): string
     {
@@ -39,46 +43,40 @@ class TabsBlockWidget extends AbstractContentWidgetType
         }
     }
 
-
-
     public function getSettingsForm(ContentWidget $contentWidget, FormFactoryInterface $formFactory): ?FormInterface
     {
         $form = $formFactory->createBuilder()
-            ->add('tab1_title',TextType::class, [
-                'label' => 'Tab 1 title',
+            ->add('tab1_title', TextType::class, [
+                'label' => 'Tab 1 Title',
                 'required' => false
             ])
-            ->add('tab1_content', TextareaType::class, [
-                'label' => 'Tab 1 Content',
-                'required' => false,
-                'attr' => ['rows' => 5]
+            ->add('tab1_content_block', ContentBlockSelectType::class, [
+                'label' => 'Tab 1 Content Block',
+                'required' => false
             ])
             ->add('tab2_title', TextType::class, [
                 'label' => 'Tab 2 Title',
                 'required' => false
             ])
-            ->add('tab2_content', TextareaType::class, [
-                'label' => 'Tab 2 Content',
-                'required' => false,
-                'attr' => ['rows' => 5]
+            ->add('tab2_content_block', ContentBlockSelectType::class, [
+                'label' => 'Tab 2 Content Block',
+                'required' => false
             ])
             ->add('tab3_title', TextType::class, [
                 'label' => 'Tab 3 Title',
                 'required' => false
             ])
-            ->add('tab3_content', TextareaType::class, [
-                'label' => 'Tab 3 Content',
-                'required' => false,
-                'attr' => ['rows' => 5]
+            ->add('tab3_content_block', ContentBlockSelectType::class, [
+                'label' => 'Tab 3 Content Block',
+                'required' => false
             ])
             ->add('tab4_title', TextType::class, [
-            'label' => 'Tab 4 Title',
-            'required' => false
+                'label' => 'Tab 4 Title',
+                'required' => false
             ])
-            ->add('tab4_content', TextareaType::class, [
-                'label' => 'Tab 4 Content',
-                'required' => false,
-                'attr' => ['rows' => 5]
+            ->add('tab4_content_block', ContentBlockSelectType::class, [
+                'label' => 'Tab 4 Content Block',
+                'required' => false
             ])
             ->getForm();
 
@@ -86,14 +84,36 @@ class TabsBlockWidget extends AbstractContentWidgetType
         return $form;
     }
 
-
-
     public function getWidgetData(ContentWidget $contentWidget): array
     {
-        return $contentWidget->getSettings();
+        $settings = $contentWidget->getSettings();
+        $tabs = [];
+
+        for ($i = 1; $i <= 4; $i++) {
+            $titleKey = "tab{$i}_title";
+            $contentBlockKey = "tab{$i}_content_block";
+
+            if (!empty($settings[$titleKey])) {
+                $contentBlockId = $settings[$contentBlockKey] ?? null;
+                $content = '';
+
+                if ($contentBlockId) {
+                    $contentBlock = $this->doctrine
+                        ->getRepository(ContentBlock::class)
+                        ->find($contentBlockId);
+
+                    if ($contentBlock) {
+                        $defaultVariant = $contentBlock->getDefaultVariant();
+                        if ($defaultVariant) {
+                            $content = $defaultVariant->getContent();
+                        }
+                    }
+                }
+
+                $settings["tab{$i}_content"] = $content;
+            }
+        }
+
+        return $settings;
     }
-
-
-
-
 }
