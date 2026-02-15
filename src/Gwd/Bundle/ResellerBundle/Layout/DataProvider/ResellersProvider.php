2b860024a6919b966469f60cd564026379d07950
Gal <galm85@gmail.com>
2026-02-15 12:34:42 +0200

    add search resellers

--- src/Gwd/Bundle/ResellerBundle/Layout/DataProvider/ResellersProvider.php
+++ src/Gwd/Bundle/ResellerBundle/Layout/DataProvider/ResellersProvider.php
@@ -4,19 +4,38 @@ namespace Gwd\Bundle\ResellerBundle\Layout\DataProvider;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Gwd\Bundle\ResellerBundle\Entity\ResellerType;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class ResellersProvider
 {
 
-    public function __construct(private EntityManagerInterface $entityManager)
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private RequestStack $requestStack
+    )
     {
 
     }
 
     public function getResellers():array
     {
-        return $this->entityManager
+        $request = $this->requestStack->getCurrentRequest();
+        $searchTerm = $request ? $request->query->get('search','') : '';
+
+        $qb = $this->entityManager
             ->getRepository(ResellerType::class)
-            ->findBy(['status'=>'active'],['name'=>'ASC']);
+            ->createQueryBuilder('r')
+            ->where('r.status = :status')
+            ->setParameter('status','active');
+
+        if($searchTerm){
+            $qb->andWhere('LOWER(r.name) LIKE LOWER(:search)')
+                ->setParameter('search','%'.$searchTerm.'%');
+        }
+
+        $qb->orderBy('r.name','ASC');
+
+        return $qb->getQuery()->getResult();
+
     }
 }
